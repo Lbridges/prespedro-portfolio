8a6ec825ec4e9dfb4b861b0547c06a02a8928c4f
LBridges <krush.art@gmail.com>
2025-03-28 00:41:03 -0400

    Update .eleventy.js input directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src");
     return {
       dir: {
-        input: "src",
-        output: "_site",
-        include: "."
+        input: ".",
+        output: "_site"
       }
     };
   };
