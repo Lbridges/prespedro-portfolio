60ede576fb657389d4f83b394a37be894af1cda9
Lauren <krush.art@gmail.com>
2025-03-31 03:32:07 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addPassthroughCopy("./assets");
 
-    // Create a 'docs' collection
-    eleventyConfig.addCollection("docs", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("src/docs/*.njk");
-    });
+  // Create a 'docs' collection
+  eleventyConfig.addCollection("docs", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/docs/*.njk");
+  });
 
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      },
-      htmlTemplateEngine: "njk"
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk"
   };
+};
