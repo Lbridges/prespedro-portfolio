5254d84c138c760b96603230c4c8dab28130a910
Lauren <krush.art@gmail.com>
2025-03-31 03:27:24 -0400

    add content

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./assets");
+
+    // Create a 'docs' collection
+    eleventyConfig.addCollection("docs", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/docs/*.njk");
+    });
+
     return {
       dir: {
         input: "src",
